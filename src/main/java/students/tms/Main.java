206dab916e19302fb85ebacc85bd2280094bfbf6
AleksandrFomin <RezviyBelorus@gmail.com>
2019-04-20 11:09:19 +0300

    github push

--- src/main/java/students/tms/Main.java
+++ src/main/java/students/tms/Main.java
@@ -1,18 +1,29 @@
 package students.tms;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
+import static java.lang.System.out;
+import static java.lang.System.setOut;
+
 public class Main {
+
     public static void main(String[] args) {
-        int i = 1;
-        int x = 6;
 
-        while (++i < --x) {
-            System.out.println("i="+i);
-            System.out.println("x="+x);
-        }
+        out.println(1.2 + 1.4);
+        out.println(Math.cos(180));
 
-        System.out.println(i);
-
+        out.println(Math.log10(100));
     }
+
+    private static int initialize(String[] args) {
+        Scanner scanner = new Scanner(args[0]);
+        if (scanner.hasNextInt()) {
+            return scanner.nextInt();
+        } else {
+            out.print('\n' + args[0] + " - that is not an INTEGER  number! value = '256' \n");
+            return 512;
+        }
+    }
+
 }
