f233a908b2e70ad10e49be7d6107ca936db340ac
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-04-20 11:25:35 +0300

    Update Main.java

--- src/main/java/students/tms/Main.java
+++ src/main/java/students/tms/Main.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 import static java.lang.System.out;
 import static java.lang.System.setOut;
 // test comment
+
+// new comment
 public class Main {
 
     public static void main(String[] args) {
